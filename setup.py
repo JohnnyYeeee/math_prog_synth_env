e93b5f8f2bf8f840b07a5c2682e74c17b27f4c8f
Johnny Ye <jye@squareup.com>
2021-07-05 15:11:01 -0400

    Adding missing requirements

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(name='dm_math_gym_env',
       version='0.0.1',
       install_requires=['gym', 'sympy', 'numpy', 'scipy', 'sentencepiece', 'torch',  'mathematics_dataset', 'tqdm',
-                        'sklearn', "google-cloud-storage", "pyyaml"]
+                        'sklearn', "google-cloud-storage", "pyyaml", "multiprocess"]
 )
